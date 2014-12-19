440788f19c952d7ce3e9df9cb9bf42c59957cb2a
eschmit <eschmit@groceryshopping.net>
2014-12-19 10:29:34 -0600

    more store functions added

--- gsnTest.php
+++ gsnTest.php
@@ -15,6 +15,17 @@
 		return $obj;
 	}
 	
+	function gsn_get_json($url) {
+	
+		$resp = wp_remote_get($url);
+		$json = NULL;
+		
+		if( 200 == $resp['response']['code']) {
+			$json = $resp['body'];
+		}
+		return $json;
+	}
+	
 	function gsn_get_featured_recipe (){
 		
 		$obj = gsn_get_data('https://clientapi.gsn2.com/api/v1/store/FeaturedRecipe/'.$GLOBALS['chainId']); 
@@ -22,10 +33,14 @@
 		return $obj->{'ImageUrl'};
 	}
 	
-	function gsn_get_store_list($chainId){
+	function gsn_get_store_list(){
 	
-		$obj = gsn_get_data('https://clientapi.gsn2.com/api/v1/store/list/'.$GLOBALS['chainId']); 
-		//return $obj[0]->{'StoreName'};
-		$GLOBALS['storeList'] = $obj;
+		$obj = gsn_get_json('https://clientapi.gsn2.com/api/v1/store/list/'.$GLOBALS['chainId']); 
+		set_transient('store_list', $obj, DAY_IN_SECONDS);
+	}
+	
+	function gsn_get_site_config() {
+		$obj = gsn_get_json('https://clientapi.gsn2.com/api/v1/store/siteconfig/'.$GLOBALS['chainId']); 
+		set_transient('site_config', $obj, DAY_IN_SECONDS);
 	}
 ?>
